12266ceb07972b82b1399e73b09cf58832d3b4c8
highnitin <nitinjoshi983@gmail.com>
2023-11-19 04:41:43 +0530

    Updated complete css using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        "1-vw":"10%",
+        "1.5vw":"1.5vw",
+        "2-vw":"20%",
+        "3-vw":"30%",
+        "4-vw":"40%",
+        "5-vw":"50%",
+        "6-vw":"60%",
+        "7-vw":"70%",
+        "8-vw":"80%",
+        "9-vw":"90%",
+      },
+    },
   },
   plugins: [],
 }
